a68662e03bdc54ec54222bf1d7c09c6fe4356dfe
Itay Kalfon <iykalfon@gmail.com>
2024-07-28 00:00:23 +0300

    fixed files

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,7 @@ import AppModule from './modules/app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as fs from 'fs';
-import path from 'path';
+import * as path from 'path';
 import * as morgan from 'morgan';
 
 export const SWAGGER_API_ROOT = 'api/docs';
@@ -12,8 +12,8 @@ export const SWAGGER_API_DESCRIPTION = 'API Description';
 
 async function bootstrap() {
   const httpsOptions = {
-    key: fs.readFileSync("../secrets/server.key"),
-    cert: fs.readFileSync("../secrets/server.crt"),
+    key: fs.readFileSync(path.join(__dirname, "..", "secrets", "server.key")),
+    cert: fs.readFileSync(path.join(__dirname, "..", "secrets", "server.crt")),
   };
   const app = await NestFactory.create(AppModule, {httpsOptions});
   app.useGlobalPipes(new ValidationPipe());
